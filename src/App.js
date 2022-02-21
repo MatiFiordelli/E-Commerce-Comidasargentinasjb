ef7d7e4126249423189b7b150cb974e0bf13a0fe
MatiFiordelli <93846274+MatiFiordelli@users.noreply.github.com>
2022-02-21 20:31:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,5 +15,3 @@ export default class App extends React.Component {
 	}
 }
 
-//cuando se navega por el historico navegacion del browser, tener en cuenta las paginas root, 
-//al ser busquedas debe actualizar el valor del input y mostrar los resultados
